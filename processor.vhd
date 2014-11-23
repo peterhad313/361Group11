6e7f228ba49c795d65d6a8754c37f65954be01de
Hannah Arntson <hannaharntson@gmail.com>
2014-11-23 14:39:53 -0600

    361Group11

--- processor.vhd
+++ processor.vhd
@@ -10,6 +10,7 @@ end processor;
 
 architecture struct of processor is
 
+--Splits instruction into useful components
 component opcode_splitter is   
 port
 (
@@ -25,7 +26,7 @@ port
 	);
 end component;
   
-  
+--Register file  
 component register_file is   
 port
 (
@@ -40,6 +41,7 @@ port
 	);
 end component;
 
+--Memory
 component syncram is
 generic (
 	mem_file : string
@@ -55,6 +57,7 @@ port (
 	);
 end component;
 
+--32 bit Mux
 component mux_32 is
   port (
 	sel   : in  std_logic;
@@ -64,6 +67,7 @@ component mux_32 is
   );
 end component;
 
+--Or gate
 component or_gate is
   port (
     x   : in  std_logic;
@@ -72,6 +76,7 @@ component or_gate is
   );
 end component;
 
+--ALU
 component alu_32 is
 	port (
 		ctrl	: in std_logic_vector(3 downto 0);	-- 
@@ -84,6 +89,7 @@ component alu_32 is
 	);
 end component;
 
+--32 bit adder
 component adder_32 is
 	port (
 		A,B	: in std_logic_vector(31 downto 0);
@@ -92,6 +98,7 @@ component adder_32 is
 	);
 end component;
 
+--32 bit D Flip-Flop
 component ddf_32 is 
   port(
     clock : in std_logic;
@@ -100,6 +107,7 @@ component ddf_32 is
   );
 end component;
 
+--Control unit
 component control is
   port (
     opcode   : in  std_logic_vector(5 downto 0); --opcode in is 6 bit input
@@ -115,32 +123,24 @@ component control is
   );
 end component;
 
+--*************************************************
 --Signals
-signal regA : std_logic_vector(31 downto 0);
-signal regB : std_logic_vector(31 downto 0);
-signal muxedRegWrite: std_logic_vector(31 downto 0);
+--*************************************************
+--PC Signals
+signal muxed_PC: std_logic_vector(31 downto 0); --Input to PC flip-flop
+signal PC_out: std_logic_vector(31 downto 0); --Next PC
+signal incremented_PC: std_logic_vector(31 downto 0); --Incremented PC
+signal branched_PC: std_logic_vector(31 downto 0); --Branched PC
+--Instruction signals
+signal instruction: std_logic_vector(31 downto 0); --Instruction, output of instruction memory
 signal selA: std_logic_vector(4 downto 0);
 signal selB: std_logic_vector(4 downto 0);
 signal writeRegSel: std_logic_vector(4 downto 0);
-signal muxed_ALU_B: std_logic_vector(31 downto 0);
-signal ALU_out: std_logic_vector(31 downto 0);
-signal opcode: std_logic_vector(5 downto 0);
-signal func: std_logic_vector( 5 downto 0);
-signal jmp_addr:  std_logic_vector(25 downto 0);
-signal immediate:  std_logic_vector(31 downto 0);
-signal shamt:  std_logic_vector(31 downto 0);
-signal cout,ovf,ze: std_logic;
-signal add_cout: std_logic;
-signal instruction: std_logic_vector(31 downto 0);
-signal InstRead, InstWrite: std_logic;
-signal memActive: std_logic;
-
-signal incremented_PC: std_logic_vector(31 downto 0);
-signal branched_PC: std_logic_vector(31 downto 0);
-signal muxed_PC: std_logic_vector(31 downto 0);
-signal PC_out: std_logic_vector(31 downto 0);
-signal readWord: std_logic_vector(31 downto 0);
-
+signal opcode: std_logic_vector(5 downto 0); --Opcode from instruction
+signal func: std_logic_vector( 5 downto 0); --Function from instruction
+signal jmp_addr:  std_logic_vector(25 downto 0); --Jump address from instruction
+signal immediate:  std_logic_vector(31 downto 0); --Add or subtract immediate
+signal shamt:  std_logic_vector(31 downto 0); --Shift amount
 --Control signals
 signal rw : std_logic; --register write
 signal mux_ALU : std_logic; --the pre-ALU mux control
@@ -150,26 +150,48 @@ signal eq  : std_logic; --branch equal
 signal memrd : std_logic; --memory read
 signal memwr  : std_logic; --memory write
 signal alu   : std_logic_vector(3 downto 0); --ALU control
+signal memActive: std_logic;
+--Register file signals
+signal regA : std_logic_vector(31 downto 0);
+signal regB : std_logic_vector(31 downto 0);
+signal muxedRegWrite: std_logic_vector(31 downto 0); --Data input to register file
+--ALU Signals
+signal muxed_ALU_B: std_logic_vector(31 downto 0); --ALU input B
+signal ALU_out: std_logic_vector(31 downto 0); --ALU output
+signal cout,ovf,ze: std_logic;
+--Adder signal
+signal add_cout: std_logic;
+--Data signals
+signal InstRead, InstWrite: std_logic;
+signal readWord: std_logic_vector(31 downto 0);
 
 begin
-
-opcode1: opcode_splitter port map (instruction, selA, selB, writeRegSel, opcode, func, jmp_addr, immediate, shamt);
-control_unit: control port map (opcode, func, rw, mux_ALU, mux_write, br, eq, memrd, memwr, alu);
-r1: register_file port map (regA,regB, muxedRegWrite, memwr,selA,selB,writeRegSel,clk);
-m1: mux_32 port map (mux_ALU, regB, immediate,muxed_ALU_B); -- alu input b
-alu1: alu_32 port map (alu, regA, muxed_ALU_B, cout, ovf, ze, ALU_out);
-or1: or_gate port map (memrd, memwr, memActive);
+--PC unit
+pc: ddf_32 port map (clk, muxed_PC, PC_out);
+--Instruction memory
 mem1: syncram 
 	generic map (mem_file=>"bills_branch.dat")
-	port map (clk, memActive, memrd, memwr, ALU_out, regB, readWord); --Data memory
-mem2: syncram 
-	generic map (mem_file=>"bills_branch.dat")
 	port map (clk, '1', '1', '0', PC_out, x"00000000", instruction); --Instruction memory
-m2: mux_32 port map (mux_write, ALU_out, readWord, muxedRegWrite); -- Write mux
-m3: mux_32 port map (br, incremented_PC, branched_PC, muxed_PC);
+--Logic to increment PC
+m1: mux_32 port map (br, incremented_PC, branched_PC, muxed_PC);
 a1: adder_32 port  map (PC_out, x"00000004", incremented_PC, add_cout);
 a2: adder_32 port map (instruction, incremented_PC, branched_PC, add_cout);
-pc: ddf_32 port map (clk, muxed_PC, PC_out);
+--Instruction splitter
+inst1: opcode_splitter port map (instruction, selA, selB, writeRegSel, opcode, func, jmp_addr, immediate, shamt);
+--Control unit
+control_unit: control port map (opcode, func, rw, mux_ALU, mux_write, br, eq, memrd, memwr, alu);
+or1: or_gate port map (memrd, memwr, memActive);
+--Register file
+r1: register_file port map (regA,regB, muxedRegWrite, memwr,selA,selB,writeRegSel,clk);
+--Mux into ALU
+m2: mux_32 port map (mux_ALU, regB, immediate,muxed_ALU_B); -- alu input b
+alu1: alu_32 port map (alu, regA, muxed_ALU_B, cout, ovf, ze, ALU_out);
+--Data memory
+mem2: syncram 
+	generic map (mem_file=>"bills_branch.dat")
+	port map (clk, memActive, memrd, memwr, ALU_out, regB, readWord); --Data memory
+--Mux to select data
+m3: mux_32 port map (mux_write, ALU_out, readWord, muxedRegWrite); -- Write mux
 
 
-end architecture ; -- strcut
+end architecture ; -- struct
